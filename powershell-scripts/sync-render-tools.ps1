cc461421a960522383b465b6009f21cc207864fb
max <max@normli.ca>
2023-06-27 12:14:30 -0400

    syncing all changes

--- powershell-scripts/sync-render-tools.ps1
+++ powershell-scripts/sync-render-tools.ps1
@@ -1,5 +1,6 @@
 git pull
 git add --all
 $message = Read-Host -Prompt 'commit message: (press enter for none)'; if ($message -eq '') {$message = 'syncing all changes'}
+Write-Host 'commit message: '$message
 git commit -m $message
 git push
