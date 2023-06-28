f9212daba0cef080762a1fa74617c563da9e7667
max <maxschramp@gmail.com>
2023-06-28 11:37:16 -0400

    syncing all changes

--- powershell-scripts/sync-3dsmax.ps1
+++ powershell-scripts/sync-3dsmax.ps1
@@ -9,8 +9,19 @@ $Shortcut = $WshShell.CreateShortcut(([Environment]::GetFolderPath("MyDocuments"
 $Shortcut.TargetPath = ($dir.FullName+'\material-libraries\')
 $Shortcut.Save()
 
-Copy-Item $dir\configs\3dsmax\init-scene.max -Destination (([Environment]::GetFolderPath("MyDocuments")+'\3ds Max 2022\materiallibraries\material-libraries.lnk'))
+# Copy-Item $dir\configs\3dsmax\init-scene.max -Destination (([Environment]::GetFolderPath("MyDocuments")+'\3ds Max 2022\materiallibraries\material-libraries.lnk'))
 
 #Copy-Item ($dir.FullName+'\lut') -Destination 'C:\Program Files\Corona\' -Container
 
-[Environment]::SetEnvironmentVariable("RENDER_TOOLS", $env:RENDER_TOOLS + $dir.FullName, "Machine")
+# [Environment]::SetEnvironmentVariable("RENDER_TOOLS", $env:RENDER_TOOLS + $dir.FullName, "Machine")
+
+<# Copy Render Settings template #>
+Copy-Item ($dir.FullName+'\configs\3dsmax\ms-corona9-renderpreset.rps') -Destination (($env:APPDATA).Replace('Roaming','Local')+"\Autodesk\3dsMax\2022 - 64bit\ENU\en-US\StartupTemplates\renderpresets")
+
+<# Generate custom paths .mxp linked to render-tools directory #>
+Write-Host 'Adding paths to project folder'
+$pathFile = (($env:APPDATA).Replace('Roaming','Local')+"\Autodesk\3dsMax\2022 - 64bit\ENU\en-US\StartupTemplates\StartupTemplates.mxp")
+$dirAll = Get-ChildItem -Path $dir -Directory -Recurse
+for ($i = 0; $i -lt $dirAll.Count; $i++) {
+    Add-Content $pathFile -Value ('Dir'+(900 + $i)+'='+$dirAll[$i].FullName)
+}
